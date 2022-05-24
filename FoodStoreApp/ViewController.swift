e5f866a6fa0634b3c664f0f1805cf1b320410a8e
Ece Ucak <62396554+eceucak@users.noreply.github.com>
2022-05-24 21:30:54 +0300

    Initial Commit

---
+++ FoodStoreApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodStoreApp
+//
+//  Created by Ece Ucak on 24.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
